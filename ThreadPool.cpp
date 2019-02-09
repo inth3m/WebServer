d6155e8a6c0e9dc0a9f8f91f2d3216089dbc333b
inth3m <lanjingu@outlook.com>
2019-02-09 10:51:44 +0800

    add threadpool

---
+++ ThreadPool.cpp
@@ -0,0 +1,13 @@
+#include"ThreadPool.h"
+
+pthread_mutex_t ThreadPool::lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t ThreadPool::notify = PTHREAD_COND_INITIALIZER;
+std::vector<pthread_t> ThreadPool::threads;
+std::vector<ThreadPoolTask> ThreadPool::queue;
+int ThreadPool::thread_count = 0;
+int ThreadPool::queue_size = 0;
+int ThreadPool::head = 0;
+int ThreadPool::tail = 0;
+int ThreadPool::count = 0;
+int ThreadPool::shutdown = 0;
+int ThreadPool::started = 0;
